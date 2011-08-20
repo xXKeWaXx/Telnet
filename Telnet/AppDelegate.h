d15b19551b245b94e359f6241a08b0ae0cbc4041
Adam Eberbach <adam@Purple-Mac-mini.local>
2011-08-20 20:34:02 +1000

    Initial Commit

---
+++ Telnet/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Telnet
+//
+//  Created by Adam Eberbach on 20/08/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Telnet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Telnet
+//
+//  Created by Adam Eberbach on 20/08/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
